8c89e3af3c57be0e966014de6c5ce55206f4cb29
Hassan Hamdy Handal <150869338+HassanHandal@users.noreply.github.com>
2025-05-20 09:40:30 +0300

    Update bot.py3

--- bot.py
+++ bot.py
@@ -26,8 +26,16 @@ MAIN_MENU = [
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     USER_STATE[update.effective_user.id] = "MAIN"
-    reply_markup = ReplyKeyboardMarkup(MAIN_MENU, resize_keyboard=True)
-    await update.message.reply_text("مرحبًا بك في الأكاديمية العلمية ✨\nاختر من القائمة التالية:", reply_markup=reply_markup)
+    chat_id = update.effective_chat.id
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo="https://hassanhandal.github.io/SAIHSBOT/start.jpg",  
+        caption=(
+            " مرحبًا بك في *الأكاديمية العلمية للدراسات الإسلامية والإنسانية ✨*\n"
+            "اختر من القائمة التالية:"
+        ),
+        parse_mode="Markdown"
+    )
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
